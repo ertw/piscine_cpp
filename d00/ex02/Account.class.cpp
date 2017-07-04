2bd18b8c31c1433bd1f47e82fa5cd08947dd7ba6
Erik Williamson <me@erik.tw>
2017-07-03 21:46:33 -0700

    start ex02

---
+++ d00/ex02/Account.class.cpp
@@ -0,0 +1,32 @@
+#include "Account.class.hpp"
+#include <iostream>
+typedef Account t;
+
+int Account::getNbAccounts(void) { return (Account::_nbAccounts); }
+int Account::getTotalAmount(void) { return (Account::_totalAmount); }
+int Account::getNbDeposits(void) { return (Account::_totalNbDeposits); }
+int Account::getNbWithdrawals(void) { return (Account::_totalNbWithdrawals); }
+void Account::displayAccountsInfos(void) {}
+
+Account::Account(int initial_deposit) { return; };
+Account::Account(void) { return; };
+Account::~Account(void) { return; };
+
+void Account::makeDeposit(int deposit){};
+bool Account::makeWithdrawal(int withdrawal) { return (false); }
+int Account::checkAmount(void) const { return (this->_amount); }
+void Account::displayStatus(void) const {}
+
+int _nbAccounts = 0;
+int _totalAmount = 0;
+int _totalNbDeposits = 0;
+int _totalNbWithdrawals = 0;
+
+void Account::_displayTimestamp(void) {
+  std::cout << "[" << __TIMESTAMP__ << "] ";
+}
+
+int _accountIndex;
+int _amount;
+int _nbDeposits;
+int _nbWithdrawals;
